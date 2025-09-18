bc9d4d6b57aefa022793a6ae212e9dd95f718474
이세환 <sehwan.lee@gintlab.com>
2025-09-18 12:01:09 +0900

    feat: 배포 스크립트

--- s3sync.sh
+++ s3sync.sh
@@ -1,1 +1,1 @@
- aws s3 sync ./ s3://www.barunchurch.org/ --exclude '.git/*' --profile bpatisserie
+ aws s3 sync ./ s3://www.barunchurch.org/ --exclude '.git/*' --profile bpatisserie && aws cloudfront create-invalidation --distribution-id E1CNUNGAZVIYKL --paths "/*" --profile bpatisserie
